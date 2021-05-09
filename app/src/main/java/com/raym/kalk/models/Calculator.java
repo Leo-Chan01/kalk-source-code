d483ee63ea6d3c34db6e44f89f56b326d7249a14
Leo-Chan01 <ephraimleo16@gmail.com>
2021-05-09 19:41:03 +0100

    Recent Changes for calculator model class
    Signed-off-by: Leo-Chan01 <ephraimleo16@gmail.com>

--- app/src/main/java/com/raym/kalk/models/Calculator.java
+++ app/src/main/java/com/raym/kalk/models/Calculator.java
@@ -1,5 +1,20 @@
 package com.raym.kalk.models;
 
 public class Calculator {
-    int creditUnit;
+    int mCreditUnit;
+    int mGradeValue;
+    float mResult;
+    int mCreditLoad;
+    int mTotalCreditUnits;
+
+
+    int calculateCreditLoad(int creditUnit, int gradeValue) {
+        mCreditLoad = creditUnit * gradeValue;
+        return mCreditLoad;
+    }
+
+    float calculateGP(int totalCreditLoad, int totalCreditUnits) {
+        mResult = (float) totalCreditLoad / totalCreditUnits;
+        return mResult;
+    }
 }
