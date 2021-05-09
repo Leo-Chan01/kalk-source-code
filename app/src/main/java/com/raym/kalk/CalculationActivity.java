dffc9306b28f2f3b833425272c1143135cfe1b7a
Leo-Chan01 <ephraimleo16@gmail.com>
2021-05-10 00:08:09 +0100

    05/10/21; 12:07PM
    Signed-off-by: Leo-Chan01 <ephraimleo16@gmail.com>

--- app/src/main/java/com/raym/kalk/CalculationActivity.java
+++ app/src/main/java/com/raym/kalk/CalculationActivity.java
@@ -3,20 +3,12 @@ package com.raym.kalk;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.raym.kalk.models.Course;
-import com.raym.kalk.models.KalkDataManager;
-
-import java.util.List;
-
 public class CalculationActivity extends AppCompatActivity {
-    private Spinner mCourseListSpinner;
     private Button mDoneButton;
     private Button mNextCourseButton;
     private Button mPreviousButton;
@@ -27,11 +19,11 @@ public class CalculationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculate);
-
-        mCourseListSpinner = findViewById(R.id.course_choice_spinner);
         mDoneButton = findViewById(R.id.done_button);
         mPreviousButton = findViewById(R.id.previous_course_button);
         mNextCourseButton = findViewById(R.id.next_course_button);
+        mCourseChoice = findViewById(R.id.edit_text_course_choice);
+        mCourseGrade = findViewById(R.id.edit_text_grade);
 
         //when the user clicks on the done button
         mDoneButton.setOnClickListener(new View.OnClickListener() {
@@ -51,10 +43,6 @@ public class CalculationActivity extends AppCompatActivity {
             }
         });
 
-        //this is the activation action for the spinner to list the items in the list of Courses
-        List<Course> courses = KalkDataManager.getInstance().getCourseArrayList();
-        ArrayAdapter<Course> adapterCourses = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, courses);
-        adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mCourseListSpinner.setAdapter(adapterCourses);
+
     }
 }
